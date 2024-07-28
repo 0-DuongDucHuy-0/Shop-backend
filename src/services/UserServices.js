c9c188ff8e7f1dd128f9e44369835557d214900e
unknown <duongduchuyaz@gmail.com>
2024-07-28 23:31:34 +0700

    check email after create

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -4,6 +4,15 @@ const createUser = (newUser) => {
     const { name, email, password, confirmPassword, phone } = newUser
     return new Promise( async (resolve, reject) => {
         try{
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if(checkUser !== null ){
+                resolve({
+                    status: 'OK',
+                    message: 'Email đã tồn tại'
+                })
+            }
             const createUser = await User.create({
                 name, 
                 email, 
