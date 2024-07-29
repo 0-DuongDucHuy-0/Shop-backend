4f04fad1996a2f1dbdda7e9e6d37ffe8da1f8ddc
unknown <duongduchuyaz@gmail.com>
2024-07-29 22:16:42 +0700

    fix user can get admin accout

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,6 +4,28 @@ dotenv.config()
 
 const authMiddleWare = (req, res, next) => {
     const token = req.headers.token.split(' ')[1]
+    const userId = req.params.id
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
+        if(err) {
+            return res.status(404).json({
+                message: 'The authentication',
+                status: 'ERROR'
+            })
+        }
+        const { payload } = user
+        if(payload?.isAdmin || payload?.id === userId){
+            next()
+        } else {
+            return res.status(404).json({
+                message: 'The authentication',
+                status: 'ERROR'
+            })
+        }
+    });
+}
+
+const authUserMiddleWare = (req, res, next) => {
+    const token = req.headers.token.split(' ')[1]
     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
         if(err) {
             return res.status(404).json({
@@ -24,5 +46,6 @@ const authMiddleWare = (req, res, next) => {
 }
 
 module.exports = {
-    authMiddleWare
+    authMiddleWare,
+    authUserMiddleWare
 }
--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const userController = require('../controllers/UserController')
-const { authMiddleWare } = require("../middleware/auth")
+const { authMiddleWare, authUserMiddleWare } = require("../middleware/auth")
 
 router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
 router.put('/uplate-user/:id', userController.uplateUser)
 router.delete('/delete-user/:id', authMiddleWare, userController.deleteUser)
 router.get('/get-all-user', authMiddleWare, userController.getAllUser)
-router.get('/get-details/:id', userController.getDetailsUser)
+router.get('/get-details/:id', authUserMiddleWare, userController.getDetailsUser)
 
 module.exports = router
