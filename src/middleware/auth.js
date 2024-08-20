0f3c2874f325b5a377995a780e8f90b83870c0dd
unknown <duongduchuyaz@gmail.com>
2024-08-20 12:17:57 +0700

    crud product

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,6 @@ dotenv.config()
 
 const authMiddleWare = (req, res, next) => {
     const token = req.headers.token.split(' ')[1]
-    const userId = req.params.id
     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
         if(err) {
             return res.status(404).json({
@@ -13,7 +12,7 @@ const authMiddleWare = (req, res, next) => {
             })
         }
         const { payload } = user
-        if(payload?.isAdmin || payload?.id === userId){
+        if(payload?.isAdmin){
             next()
         } else {
             return res.status(404).json({
@@ -26,6 +25,7 @@ const authMiddleWare = (req, res, next) => {
 
 const authUserMiddleWare = (req, res, next) => {
     const token = req.headers.token.split(' ')[1]
+    const userId = req.params.id
     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
         if(err) {
             return res.status(404).json({
@@ -34,7 +34,7 @@ const authUserMiddleWare = (req, res, next) => {
             })
         }
         const { payload } = user
-        if(payload?.isAdmin){
+        if(payload?.isAdmin || payload?.id === userId){
             next()
         } else {
             return res.status(404).json({
---
+++ src/routers/ProductRouter.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+const productController = require('../controllers/ProductController')
+const { authMiddleWare } = require("../middleware/auth")
+
+router.post('/create', productController.createProduct)
+router.put('/update/:id', productController.updateProduct)
+router.get('/details/:id', productController.getDetailsProduct)
+router.delete('/delete/:id', productController.deleteProduct)
+router.get('/get-all', productController.getAllProduct)
+
+module.exports = router
